148115b2a5953dc443945e4e72294095505e246d
KC-AN <61565331+KC-AN@users.noreply.github.com>
2020-07-13 01:48:05 +0000

    Update Roulette.js

--- Roulette.js
+++ Roulette.js
@@ -36,8 +36,8 @@
    
     a = setTimeout("diapo()",100);
     
-        alert(document.images.slide.src[3])
-    if(document.images.slide.src=="file:///C:/Users/ndiay/Documents/PROJET%20JS/images/icons8-" + nombreroulette +"-96.png")
+       
+    if(document.images.slide.src=="images/icons8-" + nombreroulette +"-96.png")
     {  
         clearTimeout(a);
         i=0;
